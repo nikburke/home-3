bb40b5ff1dadb42c862ae6d5ca2ac4a89d2860b9
nikburke <maurov.nikita@gmail.com>
2021-02-10 11:25:04 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        int randomnumber = (int) (Math.random() * 9);
+        System.out.println(game(randomnumber));
+        game2();
+
+    }
+
+    public static String game (int x) {
+        System.out.println("У вас 3 попытки");
+        Scanner sc = new Scanner(System.in);
+        for(int i = 0; i < 3; i++){
+          System.out.println("Введите число ");
+            int a = sc.nextInt();
+            if (a>x) {
+                System.out.println("Ваше число больше");
+            }
+            if (a<x) {
+                    System.out.println("Ваше число меньше");
+            }
+            if (a==x) {
+                String result = "Вы выйграли!";
+                return result;
+            }
+
+    }
+        sc.close();
+        String result = "Вы проиграли!";
+        return result;
+    }
+
+     }
